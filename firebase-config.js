55cfc66b5966a59bc593541da1fee926640bd04b
diallodona7-maker <diallodona7@gmail.com>
2026-07-20 13:26:50 +0000

    firebase-config.js
    const firebaseConfig = {
      apiKey: "AIzaSyDp-zbhR_CtQv6na7RjUXsh-ACAIJRC0fk",
      authDomain: "dona-shop-7f07d.firebaseapp.com",
      projectId: "dona-shop-7f07d",
      storageBucket: "dona-shop-7f07d.firebasestorage.app",
      messagingSenderId: "609573161624",
      appId: "1:609573161624:web:672f6a629d6b8cf9805eed",
      measurementId: "G-WWGS55P1DS"
    };

--- firebase-config.js
+++ firebase-config.js
@@ -1,2 +1,9 @@
 const firebaseConfig = {
+  apiKey: "AIzaSyDp-zbhR_CtQv6na7RjUXsh-ACAIJRC0fk",
+  authDomain: "dona-shop-7f07d.firebaseapp.com",
+  projectId: "dona-shop-7f07d",
+  storageBucket: "dona-shop-7f07d.firebasestorage.app",
+  messagingSenderId: "609573161624",
+  appId: "1:609573161624:web:672f6a629d6b8cf9805eed",
+  measurementId: "G-WWGS55P1DS"
 };
