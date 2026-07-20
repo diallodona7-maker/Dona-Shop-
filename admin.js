e39074851004b96a6f3347753d48683a7063fd69
diallodona7-maker <diallodona7@gmail.com>
2026-07-20 00:42:15 +0000

    admin.js
    document.getElementById("publier").addEventListener("click", function () {
    
    const nom = document.getElementById("nom").value;
    const prix = document.getElementById("prix").value;
    const description = document.getElementById("description").value;
    
    if (nom === "" || prix === "" || description === "") {
        alert("Veuillez remplir tous les champs.");
        return;
    }
    
    alert(
    "Produit publié !\n\n" +
    "Nom : " + nom +
    "\nPrix : " + prix + " FCFA" +
    "\nDescription : " + description
    );
    
    });

--- admin.js
+++ admin.js
@@ -1,1 +1,19 @@
-console.log("Bienvenue sur DONA SHOP ADMIN !");
+document.getElementById("publier").addEventListener("click", function () {
+
+const nom = document.getElementById("nom").value;
+const prix = document.getElementById("prix").value;
+const description = document.getElementById("description").value;
+
+if (nom === "" || prix === "" || description === "") {
+    alert("Veuillez remplir tous les champs.");
+    return;
+}
+
+alert(
+"Produit publié !\n\n" +
+"Nom : " + nom +
+"\nPrix : " + prix + " FCFA" +
+"\nDescription : " + description
+);
+
+});
