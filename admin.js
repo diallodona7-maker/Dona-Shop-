780cfe386db60e33ae96b9f5d90a9953d195a1ea
diallodona7-maker <diallodona7@gmail.com>
2026-07-20 13:16:48 +0000

    admin.js
    document.getElementById("publier").addEventListener("click", function () {
    
    let nom = document.getElementById("nom").value;
    let categorie = document.getElementById("categorie").value;
    let prix = document.getElementById("prix").value;
    let ancienprix = document.getElementById("ancienprix").value;
    let description = document.getElementById("description").value;
    let stock = document.getElementById("stock").value;
    
    if (
        nom === "" ||
        categorie === "" ||
        prix === "" ||
        description === "" ||
        stock === ""
    ) {
        alert("Veuillez remplir tous les champs obligatoires.");
        return;
    }
    
    let message =
    "Produit publié avec succès !\n\n" +
    "Nom : " + nom + "\n" +
    "Catégorie : " + categorie + "\n" +
    "Prix : " + prix + " FCFA\n" +
    "Ancien prix : " + ancienprix + " FCFA\n" +
    "Description : " + description + "\n" +
    "Quantité disponible : " + stock + "\n\n" +
    "Produit 100 % neuf\n" +
    "Livraison partout en Côte d'Ivoire.";
    
    alert(message);
    
    });

--- admin.js
+++ admin.js
@@ -1,19 +1,34 @@
 document.getElementById("publier").addEventListener("click", function () {
 
-const nom = document.getElementById("nom").value;
-const prix = document.getElementById("prix").value;
-const description = document.getElementById("description").value;
+let nom = document.getElementById("nom").value;
+let categorie = document.getElementById("categorie").value;
+let prix = document.getElementById("prix").value;
+let ancienprix = document.getElementById("ancienprix").value;
+let description = document.getElementById("description").value;
+let stock = document.getElementById("stock").value;
 
-if (nom === "" || prix === "" || description === "") {
-    alert("Veuillez remplir tous les champs.");
+if (
+    nom === "" ||
+    categorie === "" ||
+    prix === "" ||
+    description === "" ||
+    stock === ""
+) {
+    alert("Veuillez remplir tous les champs obligatoires.");
     return;
 }
 
-alert(
-"Produit publié !\n\n" +
-"Nom : " + nom +
-"\nPrix : " + prix + " FCFA" +
-"\nDescription : " + description
-);
+let message =
+"Produit publié avec succès !\n\n" +
+"Nom : " + nom + "\n" +
+"Catégorie : " + categorie + "\n" +
+"Prix : " + prix + " FCFA\n" +
+"Ancien prix : " + ancienprix + " FCFA\n" +
+"Description : " + description + "\n" +
+"Quantité disponible : " + stock + "\n\n" +
+"Produit 100 % neuf\n" +
+"Livraison partout en Côte d'Ivoire.";
+
+alert(message);
 
 });
