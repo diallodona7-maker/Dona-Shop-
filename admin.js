be7d0e25ba75fae3863f96d29f5e23a7507abec1
diallodona7-maker <diallodona7@gmail.com>
2026-07-26 00:36:53 +0000

    admin.js
    document.addEventListener("DOMContentLoaded", ()=>{
    
    const bouton=document.getElementById("publierProduit");
    
    if(bouton){
    
    bouton.addEventListener("click",()=>{
    
    alert("Produit publié avec succès !");
    
    });
    
    }
    
    });

--- admin.js
+++ admin.js
@@ -1,41 +1,15 @@
-document.getElementById("publier").addEventListener("click", function () {
+document.addEventListener("DOMContentLoaded", ()=>{
 
-let nom = document.getElementById("nom").value;
-let categorie = document.getElementById("categorie").value;
-let prix = document.getElementById("prix").value;
-let ancienprix = document.getElementById("ancienprix").value;
-let description = document.getElementById("description").value;
-let stock = document.getElementById("stock").value;
+const bouton=document.getElementById("publierProduit");
 
-if (
-    nom === "" ||
-    categorie === "" ||
-    prix === "" ||
-    description === "" ||
-    stock === ""
-) {
-    alert("Veuillez remplir tous les champs obligatoires.");
-    return;
-}
+if(bouton){
 
-let message =
-"Produit publié avec succès !\n\n" +
-"Nom : " + nom + "\n" +
-"Catégorie : " + categorie + "\n" +
-"Prix : " + prix + " FCFA\n" +
-"Ancien prix : " + ancienprix + " FCFA\n" +
-"Description : " + description + "\n" +
-"Quantité disponible : " + stock + "\n\n" +
-"Produit 100 % neuf\n" +
-"Livraison partout en Côte d'Ivoire.";
+bouton.addEventListener("click",()=>{
 
-alert(message);
+alert("Produit publié avec succès !");
 
 });
-document
-.getElementById("publierProduit")
-.addEventListener("click", function () {
 
-alert("Votre produit a été publié avec succès !");
+}
 
 });
